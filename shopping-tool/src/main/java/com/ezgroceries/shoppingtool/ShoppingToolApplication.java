67647dc7fea1084fa20d6fcb5a9752d008d2cc5b
els van impe <els.vanimpe@kbc.be>
2022-05-05 11:41:22 +0200

    testels

---
+++ shopping-tool/src/main/java/com/ezgroceries/shoppingtool/ShoppingToolApplication.java
@@ -0,0 +1,13 @@
+package com.ezgroceries.shoppingtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingToolApplication.class, args);
+    }
+
+}
