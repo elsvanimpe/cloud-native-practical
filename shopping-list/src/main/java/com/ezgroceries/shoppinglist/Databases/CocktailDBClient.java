526896e55f02d0d020e1c5bfd422ba4ba5f96f63
els van impe <els.vanimpe@kbc.be>
2022-05-20 14:38:58 +0200

    lab04

---
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/Databases/CocktailDBClient.java
@@ -0,0 +1,19 @@
+package com.ezgroceries.shoppinglist.Database;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Component
+@FeignClient(name = "cocktailDBClient", url = "https://www.thecocktaildb.com/api/json/v1/1")
+// an implementation class is not necessary. @component zorgt ervoor dat die autowired werkt, @feign maakt de implementatie van je interface
+public interface CocktailDBClient {
+
+    @GetMapping(value = "search.php")
+    com.ezgroceries.shoppinglist.Database.CocktailDBResponse searchCocktails(@RequestParam("s") String search);
+
+    @GetMapping(value = "search.php")
+    com.ezgroceries.shoppinglist.Database.CocktailDBResponse searchAllCocktails();
+
+}
--- shopping-list/src/test/java/com/ezgroceries/shoppinglist/TestCocktailRESTController.java
+++ shopping-list/src/test/java/com/ezgroceries/shoppinglist/TestCocktailRESTController.java
@@ -1,15 +1,10 @@
 package com.ezgroceries.shoppinglist;
-
-import com.ezgroceries.shoppinglist.Controllers.CocktailController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -47,6 +42,7 @@ public class TestCocktailRESTController {
                         .content("[{\"name\": \"Elise's birthday\"},{\"name\": \"Maarten's birthday\"}]"))
                 .andDo(print())
                 .andExpect(status().isCreated());
+
     }
 
     public static String asJsonString(final Object obj) {
